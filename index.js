e7a83670ef63f0d54908032702b5dd8f46f34758
aligbillow <112569611+aligbillow@users.noreply.github.com>
2023-02-02 10:10:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import './index.css';
-import App from './App';
+import App from './containers/App';
 // import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-
 
 
 const root = createRoot(document.getElementById('root'));
