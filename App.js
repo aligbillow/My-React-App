efe14e42eda125770a311983570db14f83a5231a
aligbillow <112569611+aligbillow@users.noreply.github.com>
2023-02-02 10:10:04 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'; 
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
+// import App from  './containers/App'
 
 class App extends Component {
     constructor() {
